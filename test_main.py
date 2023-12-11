d3abd2e342b094e1c29e91935a8d562b24588a5b
Xinyi Sheng <xs110@duke.edu>
2023-12-11 15:43:06 -0500

    initial

--- test_main.py
+++ test_main.py
@@ -1,19 +1,10 @@
-import unittest
-from unittest.mock import patch
-import main
+"""
+Test goes here
+
+"""
+
+from mylib.calculator import add
 
 
-class Test_run_query(unittest.TestCase):
-    @patch("builtins.print")  # Mock the print function to capture output
-    def test_run_query(self, mock_print):
-
-        # Call the connect_to_database function
-        main.run_query()
-
-        # Assertions based on the expected output
-        mock_print.assert_any_call("Query execution completed.")
-        mock_print.assert_any_call("Connection closed.")
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add():
+    assert add(1, 2) == 3
